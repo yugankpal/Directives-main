5a1a35e8002421953a61f9ebd81246b70ed97ae8
Yugank Pal <yugankpal@gmail.com>
2022-06-23 12:46:58 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'App';
+  num = [20, 9, 10, 11, 12, 13, 14, 15];
+}
